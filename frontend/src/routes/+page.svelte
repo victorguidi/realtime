bbcf63854aa6f83b21852d58d16a013bc6ed3e5b
victor.guidi <vsantosg4072@gmail.com>
2023-04-29 18:28:03 -0300

    chat is now working

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,38 +1,64 @@
 <script lang="ts">
+	import { writable } from 'svelte/store';
+
 	type Message = {
+		where?: string;
 		from: number;
 		message: string;
 	};
-
 	let message: Message;
-	$: messages = [];
+	let id = writable<number>(0);
+	let sessionId: number;
+	let messages = writable<Message[]>([]);
+	let socket: WebSocket;
 
-	function openConnection(): void {
-		const socket = new WebSocket('wss://localhost:3000/wss/login', ['1']);
+	$: id.subscribe((val) => {
+		if (val !== 0) {
+			sessionId = Number(val);
+			openConnection(Number(val));
+		}
+	});
+
+	function openConnection(id: number): void {
+		console.log(id);
+		socket = new WebSocket('wss://localhost:3000/wss/login', ['1']);
 		socket.addEventListener('open', function (event) {
 			socket.send(
 				JSON.stringify({
 					sessionToken: 'tes',
-					id: 1
+					id: id
 				})
 			);
 		});
 		socket.addEventListener('message', function (event) {
 			if (!event.data.includes('User authenticated')) {
+				console.log(event.data);
 				const msg = JSON.parse(event.data);
-				console.log(msg.from);
-				messages.push({ from: msg.from, message: msg.message } as Message);
+				if (msg.message === 'close') {
+					socket.close();
+					return;
+				}
+				message = { from: msg.from, message: msg.message } as Message;
+				messages.update((msgs) => [...msgs, message]);
 			}
 		});
 	}
 
-	// let arr = [[{ sender: [] }, { receiver: [] }]];
+	function sendMessage(): void {
+		socket.send(
+			JSON.stringify({
+				from: sessionId,
+				message: document.getElementById('sendbox')?.value as string
+			})
+		);
+		document.getElementById('sendbox')!.value = '';
+	}
 </script>
 
-<div class="flex flex-col w-screen h-screen">
+<div class="flex flex-col w-screen h-screen bg-zinc-950 p-1 text-white">
 	<nav class="w-full h-12">Header</nav>
 	<div class="flex w-full items-center justify-around h-full">
-		<div class="flex flex-col bg-red-300 w-1/6 h-full p-4">
+		<div class="flex flex-col w-1/6 h-full p-4">
 			<h1>Sessions</h1>
 			<div class="flex p-1">
 				<div>Image</div>
@@ -43,32 +69,64 @@
 				<h1>Add</h1>
 				<h1>Create</h1>
 			</div>
-			<button on:click={openConnection}>Open</button>
+			<input type="number" name="id" id="id" class="text-black" />
+			<button on:click={() => id.set(Number(document.getElementById('id')?.value))}>Open</button>
 		</div>
-		<div class="flex flex-col bg-blue-400 w-5/6 h-full justify-around p-4">
-			<div class="flex flex-col p-4 w-full h-full bg-yellow-400">
-				<button on:click={() => console.log(messages)}>print</button>
-				{#each messages as msg}
-					<p>{msg.message}</p>
-				{/each}
-				<!-- {#each arr as item} -->
-				<!-- 	<div class="flex flex-row justify-between"> -->
-				<!-- 		<div class="flex flex-col justify-start bg-purple-300 w-1/2"> -->
-				<!-- 			{#each item[0].sender as sender} -->
-				<!-- 				<div>{sender.message.message}</div> -->
-				<!-- 			{/each} -->
-				<!-- 			<br /> -->
-				<!-- 		</div> -->
-				<!-- 		<div class="flex flex-col justify-end bg-green-300 w-1/2"> -->
-				<!-- 			<br /> -->
-				<!-- 			{#each item[1].receiver as receiver} -->
-				<!-- 				<div>{receiver.message}</div> -->
-				<!-- 			{/each} -->
-				<!-- 		</div> -->
-				<!-- 	</div> -->
-				<!-- {/each} -->
+		<div class="flex flex-col bg-zinc-900 w-5/6 h-full justify-around p-4 relative">
+			<div class="flex flex-row p-4 w-full h-full pr-6">
+				<div class="flex flex-col w-1/2 h-full">
+					{#each $messages as msg}
+						{#if msg.from == sessionId}
+							<div class="flex justify-end rounded-md p-2 h-24" />
+							<!-- <br /> -->
+						{/if}
+						{#if msg.from != sessionId}
+							<div
+								class="flex items-center bg-indigo-800 max-w-prose rounded-md p-2 h-24 max-h-full"
+							>
+								<p class="text-white break-words">{msg.message}</p>
+							</div>
+							<br />
+						{/if}
+						{#if msg.from == sessionId}
+							<div class="flex justify-end rounded-md p-2 h-24" />
+							<!-- <br /> -->
+						{/if}
+						<!-- <div class="flex justify-end rounded-md p-2 h-24" /> -->
+					{/each}
+				</div>
+				<div />
+				<div class="flex flex-col w-1/2 h-full items-end">
+					{#each $messages as msg}
+						{#if msg.from != sessionId}
+							<div class="flex justify-end rounded-md p-2 h-24" />
+							<!-- <br /> -->
+						{/if}
+						{#if msg.from == sessionId}
+							<div
+								class="flex items-center bg-indigo-800 max-w-prose rounded-md p-2 h-24 max-h-full"
+							>
+								<p class="text-white break-words">{msg.message}</p>
+							</div>
+							<br />
+						{/if}
+						{#if msg.from != sessionId}
+							<div class="flex justify-end rounded-md p-2 h-24" />
+							<!-- <br /> -->
+						{/if}
+					{/each}
+				</div>
 			</div>
-			<div class="flex w-full h-1/4 bg-cyan-500">type</div>
+			<div class="flex w-full p-2 bg-cyan-900">
+				<textarea
+					type="text"
+					name="sendbox"
+					id="sendbox"
+					value=""
+					class="flex w-full h-14 rounded-md text-black"
+				/>
+				<button on:click={sendMessage}>Send</button>
+			</div>
 		</div>
 	</div>
 </div>
